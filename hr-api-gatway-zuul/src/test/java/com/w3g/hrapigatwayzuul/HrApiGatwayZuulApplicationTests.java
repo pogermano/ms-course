66d0b79b2973a5840e3fdab263e5c9abd3b9f9bb
pogermano <pogermano@gmail.com>
2022-01-09 15:48:30 -0300

    Project hr-gateway-zuul created

---
+++ hr-api-gatway-zuul/src/test/java/com/w3g/hrapigatwayzuul/HrApiGatwayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w3g.hrapigatwayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatwayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
